d83268c786ded8a5bc34cdeb479bee0af9c3ef77
Didarul Shahriar <didarul13579@gmail.com>
2025-11-25 00:17:38 +0600

    added mongodb connection

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.spgu1hn.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("phone_kinoo");
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('PhoneKinoo Server is Running');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
